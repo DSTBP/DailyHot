7b88e4f774ff74d136b225439d4c913b34d21631
DSTBP <taddeowang123@gmail.com>
2026-02-17 18:11:52 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: DSTBP taddeowang123@gmail.com
+ * @Date: 2026-02-17 16:04:12
+ * @LastEditors: DSTBP taddeowang123@gmail.com
+ * @LastEditTime: 2026-02-17 18:10:42
+ * @FilePath: \DailyHot\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
@@ -75,6 +83,14 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 6699,
+      // 👇 【新增代码】：本地开发环境的代理配置，用于绕过跨域限制
+      proxy: {
+        "/newsnow-api": {
+          target: "https://newsnow.busiyi.world",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/newsnow-api/, "/api")
+        }
+      }
     },
     build: {
       minify: "terser",
