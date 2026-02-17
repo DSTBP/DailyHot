b12704c32a8366b9e898fc93fe6d8cc23ed43772
DSTBP <taddeowang123@gmail.com>
2026-02-17 18:47:02 +0800

    Update

--- src/api/index.js
+++ src/api/index.js
@@ -75,7 +75,7 @@ export const getHotLists = async (type, isNew = false, params) => {
       }
     } catch (newsnowError) {
       console.error(`[${type}] 备用源同样请求失败:`, newsnowError);
-      return { code: 500, title: "获取失败", message: `：${type}数据源不可用` };
+      return { code: 500, title: "获取失败", message: `：${type} 数据源不可用` };
     }
   }
 };
